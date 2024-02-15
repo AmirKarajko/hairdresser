bb9fc146b01704a3b850d79353b2d447bedabb34
AmirKarajko <amir.karajko@gmail.com>
2024-02-15 19:53:15 +0100

    #25 Add Login Page

---
+++ mypackage/login.go
@@ -0,0 +1,35 @@
+package mypackage
+
+import (
+    "log"
+	"html/template"
+    "net/http"
+)
+
+type LoginPageData struct {
+	Title string
+	Content string
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	data := IndexPageData {
+		Title: "Hairdresser | Login",
+		Content: "This is a hairdresser web application.",
+	}
+
+	tmpl, err := template.ParseFiles("pages/login.html")
+
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
